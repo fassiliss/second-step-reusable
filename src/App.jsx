5a1670d0cc2ea714e644ea70e1cd503e72f3cae8
fassiliss <fassiliss@gmail.com>
2025-12-31 13:09:02 -0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -47,20 +47,13 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handlePrevious}
-              text="Previous"
-            />
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
-              onClick={handleNext}
-              text="Next"
-            />
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
+              <span>👈🏾</span> Previous
+            </Button>
 
-           
+            <Button bgColor="#7950f2" textColor="#fff" onClick={handleNext}>
+              Next <span>👉🏾</span>
+            </Button>
           </div>
         </div>
       )}
@@ -68,13 +61,13 @@ function Steps() {
   );
 }
 
-function Button({ textColor, bgColor, onClick, text }) {
+function Button({ textColor, bgColor, onClick, children }) {
   return (
     <button
       style={{ backgroundColor: bgColor, color: textColor }}
       onClick={onClick}
     >
-      {text}
+      {children}
     </button>
   );
 }
